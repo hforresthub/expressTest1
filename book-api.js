fd5d1d3d21d4c5f2e95b40ef11b518998df6086b
Hal <hgrforrest@gmail.com>
2022-01-07 14:16:07 -0500

    added form

---
+++ book-api.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
+
+//data
+let books = []
+
+app.use(cors())
+
+//configure body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+app.post('/book', (req, res) => {
+	const book = req.body
+
+	console.log(book)
+	books.push(book)
+})
+
+
+app.listen(port, () => {
+	console.log(`hi world app is listening on port ${port}`)
+})
